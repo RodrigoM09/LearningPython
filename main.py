dc53cd85ef63cc7de194b49365cc035ae2123250
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-04 11:20:02 -0600

    BeginnerPython

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 #String length ------------->
 print(len(input("What is your name? ")))
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+
+#Band Name Generator --------->
+city = input("What is the name of the city you grew up in?\n")
+
+pet = input("What's your pets name?\n")
+
+bandname = print(city + pet)
+
+#Adding 2 numbers from same input ------>
+two_digit_number = input("Type a two digit number: ")
+
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
