8fdd7eecb1aeb1c46c42c345c641651ad2699ba2
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-13 08:38:17 -0800

    bidding

--- main.py
+++ main.py
@@ -601,7 +601,14 @@ bids = {}
 BIDDING_FINISHED = False
 
 def find_highest_bidder(bidding_record):
-    
+    """Function printing python version."""
+    highest_bid = 0
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of {highest_bid}")
 
 while not BIDDING_FINISHED:
     name = input("What is your name?: ")
@@ -613,4 +620,4 @@ while not BIDDING_FINISHED:
     elif should_continue == "yes":
         os.system('clear')
 
-print(f"The winner is {name} with a bid of {bid}")
+find_highest_bidder(bids)
