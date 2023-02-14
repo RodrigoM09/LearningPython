09e7a25e1f812ec748f284752eb1bf264abdfe3b
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-02-14 09:39:22 -0800

    day12

--- main.py
+++ main.py
@@ -686,19 +686,25 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("Whats the first number?: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("Whats the second number?: "))
-calculation_function = operations[operation_symbol]
-first_answer = calculation_function(num1, num2)
+def calculator():
+    """Calculator app"""
+    num1 = int(input("Whats the first number?: "))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
 
-print(f"{num1} {operation_symbol} {num2} = {first_answer}")
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("Whats the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
 
-operation_symbol = input("Pick another operation: ")
-num3 = int(input("What's the next number?: "))
-calculation_function = operations[operation_symbol]
-second_answer = calculation_function(calculation_function(num1, num2), num3)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-print(f"{first_answer} {operation_symbol} {num3} = {second_answer}")
+        if input(f"Type 'y' to continue calculating with {answer} or type 'n' to exit.: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
